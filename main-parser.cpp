69ff16dd0dd2f132b09cd2ce917c3887e208fa2a
Antonin Delpeuch <antonin@delpeuch.eu>
2013-12-12 18:27:27 +0100

    Added a simple main for all this.

---
+++ main-parser.cpp
@@ -0,0 +1,44 @@
+
+#include "lexicon.h"
+#include "parser.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if(argc != 2)
+	{
+	    cerr << "Usage:\n"
+		 << argv[0] << " [lexicon]\n"
+		 << endl;
+	    return 1;
+	}
+
+	Lexicon lex(argv[1]);
+
+	while(cin.good())
+	{
+		string line;
+		getline(cin, line);
+
+		istringstream inputWords(line);
+		list<string> sentence;
+		string currentWord;
+		while(inputWords.good())
+		{
+			inputWords >> currentWord;
+			sentence.push_back(currentWord);
+		}
+
+		SimpleType sType("s",0);
+		FrameString fs(lex, sentence, sType);
+
+		SPGParser parser(fs);
+		cout << parser.run() << endl;
+	}
+
+	return 0;
+}
+
